61cdddb7d51f584977605c774c70779bfd8d8482
Gourav <55048245+Gourav0407@users.noreply.github.com>
2026-08-18 23:40:54 +0530

    Redis, Mailing, Scheduler,MongoTemplate

--- src/test/java/net/engineeringdigest/journalApp/services/RedisServiceTest.java
+++ src/test/java/net/engineeringdigest/journalApp/services/RedisServiceTest.java
@@ -1,11 +1,13 @@
 package net.engineeringdigest.journalApp.services;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+@Disabled
 @SpringBootTest
 public class RedisServiceTest {
 
--- src/test/java/net/engineeringdigest/journalApp/services/UserCustomRepoImplTest.java
+++ src/test/java/net/engineeringdigest/journalApp/services/UserCustomRepoImplTest.java
@@ -2,11 +2,13 @@ package net.engineeringdigest.journalApp.services;
 
 import net.engineeringdigest.journalApp.repository.UserCustomRepoImpl;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
+@Disabled
 @SpringBootTest
 public class UserCustomRepoImplTest {
 
